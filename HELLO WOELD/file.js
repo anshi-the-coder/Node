00f185c06cd308bba3e95ce7d9a0f384dd0bda61
anshi-the-coder <anshikagangwar341@gmail.com>
2025-08-10 21:34:06 +0530

    How to Work Nodejs

--- HELLO WOELD/file.js
+++ HELLO WOELD/file.js
@@ -1,10 +1,28 @@
 const fs = require("fs");
+const os = require("os");
 
-//Sync..
+console.log(os.cpus().length);
+//Sync..Blocking
 // fs.writeFileSync('./test.txt','Hello World');    // return something
 
-//Async
+//Async....Non Blocking req
 // fs.writeFile("./test.txt", "Hello World Async", (err)=>{});  // alwasy expect callback
+
+// console.log(1);
+
+//Blocking
+// const result =fs.readFileSync('contacts.txt','utf-8');
+// console.log(result);
+// console.log(2);
+
+// console.log(1);
+//Non Blocking
+const result = fs.readFile('contacts.txt','utf-8',(err,result)=>{
+    // console.log(result);
+})
+// console.log(2);
+
+//Default Thread Pool Size =4
 
 // const result=fs.readFileSync("./contacts.txt", "utf-8");
 // console.log(result);
@@ -17,10 +35,10 @@ const fs = require("fs");
 //     }
 // })
 
-fs.appendFileSync("./test.txt",`${Date.now()}Hey There\n`);
+// fs.appendFileSync("./test.txt",`${Date.now()}Hey There\n`);
 
 // fs.cpSync('./test.txt', './copy.txt')
 
 // fs.unlinkSync("./copy.txt")
-console.log(fs.statSync('./test.txt').isFile())
-fs.mkdirSync('my-docs/a/b', {recursive:true});
+// console.log(fs.statSync('./test.txt').isFile())
+// fs.mkdirSync('my-docs/a/b', {recursive:true});
