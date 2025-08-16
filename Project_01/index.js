8ea2000379bb2c41dd8ff2c5fe828b33d1a8fdaa
anshi-the-coder <anshikagangwar341@gmail.com>
2025-08-16 22:23:36 +0530

    Introduction to POSTMAN for REST API's

--- Project_01/index.js
+++ Project_01/index.js
@@ -1,40 +1,49 @@
 const express = require("express");
-const users = require("../MOCK_DATA.json")
+const fs = require("fs")
+const users = require("../MOCK_DATA.json");
+
 const app = express();
 const PORT = 8000;
 
 
-app.get("/users",(req,res)=>{
-    const html=`
+app.use(express.urlencoded({extended: false})); // Middleware to parse JSON
+
+app.get("/users", (req, res) => {
+    const html = `
     <ul>
-    ${users.map(user =>`<li>${user.first_name}</li>`).join("")}
+    ${users.map(user => `<li>${user.first_name}</li>`).join("")}
     </ul>
     `;
     res.send(html);
-})
-//Routes
-app.get("/api/users",(req,res)=>{
+});
+
+// GET all users
+app.get("/api/users", (req, res) => {
     return res.json(users);
 });
 
-app.route("/api/users/:id").get("/api/users/:id",(req,res)=>{
-    const id= Number(req.params.id);
-    const user= users.find((user)=> user.id===id);
+// GET / PATCH / DELETE by ID
+app.route("/api/users/:id")
+.get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
     return res.json(user);
 })
-.patch((req,res)=>{
-    return  res.json({status: "Panding"})
+.patch((req, res) => {
+    return res.json({ status: "pending" });
 })
-.delete((req,res)=>{
-    return  res.json({status: "Panding"})
+.delete((req, res) => {
+    return res.json({ status: "pending" });
 });
-app.post("/api/users",(req,res)=>{
-   return  res.json({status: "Panding"});
+
+// POST new user
+app.post("/api/users", (req, res) => {
+    const body = req.body;
+ 
+    users.push({ ...body,id: users.length + 1});
+    fs.writeFile("../MOCK_DATA.json", JSON.stringify(users),(err,data)=>{
+   return res.json({ status: "success", id: users.length});
+    })
 });
-// app.patch("/api/users:id",(req,res)=>{
-//    return  res.json({status: "Panding"});
-// });
-// app.delete("/api/users:id",(req,res)=>{
-//    return  res.json({status: "Panding"});
-// });
-app.listen(PORT,()=>console.log(`Server Started at PORT:${PORT}`))
+
+app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`));
