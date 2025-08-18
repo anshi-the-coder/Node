f4b9ad0887575dcba60b4e07fe5ec25540897cb8
anshi-the-coder <anshikagangwar341@gmail.com>
2025-08-18 20:56:49 +0530

    Status code

--- Project_01/index.js
+++ Project_01/index.js
@@ -36,8 +36,8 @@ app.get("/users", (req, res) => {
 
 // GET all users
 app.get("/api/users", (req, res) => {
-res.setHeader("X-MyName", "is Anshika Gangwar");
-// always add x to custom headers
+  res.setHeader("X-MyName", "is Anshika Gangwar");
+  // always add x to custom headers
 
   // res.setHeader('MyName','Anshika Gangwar')
   // console.log("I am in get route", req.myUserName)
@@ -50,6 +50,7 @@ app
   .get((req, res) => {
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
+    if(!user) return res.status(404).json({error: 'user not found'})
     return res.json(user);
   })
   .patch((req, res) => {
@@ -62,10 +63,19 @@ app
 // POST new user
 app.post("/api/users", (req, res) => {
   const body = req.body;
-
+  if (
+    !body ||
+    !body.first_name ||
+    !body.last_name ||
+    !body.email ||
+    !body.gender ||
+    !body.job_title
+  ) {
+    return res.status(400).json({ msg: "All fields are required" });
+  }
   users.push({ ...body, id: users.length + 1 });
   fs.writeFile("../MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.json({ status: "success", id: users.length });
+    return res.status(201).json({ status: "success", id: users.length });
   });
 });
 
